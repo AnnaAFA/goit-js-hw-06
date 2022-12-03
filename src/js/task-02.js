e98c93d25fb0f3c27025bc72e9a2c806ca77927a
AnnaAFA <anna.fedoruk111@gmail.com>
2022-12-03 15:50:22 +0200

    2 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,46 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// const itemRef = document.createElement('li');
+// itemRef.textContent = ingredients[0];
+// itemRef.classList.add('item');
+// console.log(itemRef);
+
+const listRef = document.querySelector('#ingredients');
+// console.log(listRef);
+
+// ------------------------------------
+
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+// console.log(ingredients[i]);
+//   const itemRef = document.createElement('li');
+//   itemRef.textContent = ingredients[i];
+//   itemRef.classList.add('item');
+//   elements.push(itemRef);
+// }
+// console.log(elements);
+// listRef.append(...elements);
+
+// -------------------------------------
+
+const elements = ingredients.map(el => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = el;
+  itemRef.classList.add('item');
+  return itemRef;
+});
+console.log(elements);
+listRef.append(...elements);
+
+// --------------------------------------
+
+// const makeMarkup = markup => {
+//   return markup.map(el => {
+//     const itemRef = document.createElement('li');
+//     itemRef.textContent = el;
+//     itemRef.classList.add('item');
+//     return itemRef;
+//   });
+// };
+// const elements = makeMarkup(ingredients);
+// listRef.append(...elements);
