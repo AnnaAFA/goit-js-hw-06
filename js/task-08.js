48f52287470c6cfaa156838c2c35680603b261be
AnnaAFA <anna.fedoruk111@gmail.com>
2022-12-07 18:18:33 +0200

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formElements = e.currentTarget.elements;
+
+  if (!formElements.email.value || !formElements.password.value) {
+    alert('Заповніть всі поля!');
+    return;
+  }
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
