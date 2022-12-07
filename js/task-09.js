b801370b886393663868cfc4059ea0be8ef1e54c
AnnaAFA <anna.fedoruk111@gmail.com>
2022-12-07 18:49:50 +0200

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const colorNumber = document.querySelector('.color');
+// console.log(colorNumber);
+const changeBtn = document.querySelector('.change-color');
+
+changeBtn.addEventListener('click', onChangeBtn);
+
+function onChangeBtn() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  colorNumber.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorNumber.textContent;
 }
